072ec588817aff03f0119610de8f13eeb66b699c
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-16 15:25:30 -0800

    Update js

--- assets/script.js
+++ assets/script.js
@@ -42,6 +42,30 @@ class UI {
 
    list.appendChild(row);
   }
+  //delete books
+  static deleteBook(el) {
+    if(el.classList.contains('delete')) {
+       el.parentElement.parentElement.remove();
+    }
+  }
+
+  static showAlert(message, className) {
+   const div = document.createElement('div');
+   div.className = `alert alert-${className}`;
+   div.appendChild(document.createTextNode(message));
+   const container = document.querySelector('.container');
+   const form = document.querySelector('#book-form');
+   container.insertBefore(div, form);
+   //vanish in 3 seconds
+   setTimeout(() => document.querySelector('.alert').remove(),3000);
+  }
+
+//clear field
+  static clearFields() {
+    document.querySelector('#title').value='';
+    document.querySelector('#author').value='';
+    document.querySelector('#isbn').value='';
+  }
 }
 //Store Class: Handles Storage
 
@@ -49,4 +73,32 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 //event add books
 
+document.querySelector('#book-form').addEventListener('submit', (e) =>{
+  //Prevent actual submit
+  e.preventDefault();
+//Get form values
+const title = document.querySelector('#title').value;
+const author = document.querySelector('#author').value;
+const isbn = document.querySelector('#isbn').value;
+//validate
+if(title === '' || author === ''|| isbn ==='') {
+   UI.showAlert('Please fill in all fields', 'danger');
+} else {
+  //instatiat book
+const book = new Book(title, author, isbn);
+
+//Add Book to UI
+UI.addbookToList(book);
+
+//Show success message
+UI.showAlert('Book added', 'success');
+//clear field
+UI.clearFields();
+
+}
+
+});
 //event :Remove book
+document.querySelector('#book-list').addEventListener('click',(e)=>{
+UI.deleteBook(e.target)
+});
